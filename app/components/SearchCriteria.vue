51b8e26c6a4485d49f1ab9b34c5079d2e364ce47
JonathanGuillenL <jonathanguillenlainez22@gmail.com>
2025-09-01 21:19:41 -0600

    Crear componente SearchCriteria

---
+++ app/components/SearchCriteria.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = defineProps<{
+  items: CriteriaItem[]
+}>()
+
+const emit = defineEmits(['search', 'clear'])
+
+const criteria = defineModel<string | null>('criteria')
+const criteriaData = defineModel<string | null>('criteriaValue')
+const criterioLabel = computed(() => {
+  const label = props.items.find(item => item.value == criteria.value)
+  return label?.title
+})
+
+function clearCriteriaHandler() {
+  criteria.value = null
+  emit('clear')
+}
+</script>
+
+<template>
+  <div class="flex flex-wrap gap-x-2">
+    <VSelect
+      v-model="criteria"
+      :items="items"
+      class="w-60"
+      label="Criterio de búsqueda"
+      clearable
+      variant="solo"
+      density="compact"
+      @click:clear="clearCriteriaHandler"
+      @update:model-value="criteriaData = null"
+    />
+    <VTextField
+      v-if="criteria"
+      v-model="criteriaData"
+      :label="criterioLabel"
+      class="w-52"
+      variant="outlined"
+      density="compact"
+    />
+    <button
+      class="font-semibold text-sm text-white bg-blue-500 rounded-md hover:shadow-lg px-3 py-2 mb-6"
+      @click="$emit('search')"
+    >
+      <VIcon icon="fa-solid fa-magnifying-glass" size="small" />
+    </button>
+  </div>
+</template>
